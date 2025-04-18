c5417f9946663bba36a9bca76c4885eb1ed5c8ba
yorliabdulai <yorliabdulai1@gmail.com>
2025-04-18 19:20:58 +0000

    fix: add '@safe-globalThis/safe-apps-sdk' to external dependencies in rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      external: ['@safe-globalThis/safe-apps-sdk'],
       plugins: [
         rollupNodePolyFill(), 
       ],
